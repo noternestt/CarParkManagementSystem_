9989aad36491829523aa996b8ef0df58d7ad59c7
noternestt <2305050c@student.tp.edu.sg>
2024-12-08 19:10:16 +0800

    Added seed role and user

--- CarParkManagementSystem/Data/CarParkManagementSystemUser.cs
+++ CarParkManagementSystem/Data/CarParkManagementSystemUser.cs
@@ -5,5 +5,7 @@ namespace CarParkManagementSystem.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class CarParkManagementSystemUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
--- CarParkManagementSystem/Migrations/20241208110814_SeedUserRole.Designer.cs
+++ CarParkManagementSystem/Migrations/20241208110814_SeedUserRole.Designer.cs
@@ -4,6 +4,7 @@ using CarParkManagementSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarParkManagementSystem.Migrations
 {
     [DbContext(typeof(CarParkManagementSystemContext))]
-    partial class CarParkManagementSystemContextModelSnapshot : ModelSnapshot
+    [Migration("20241208110814_SeedUserRole")]
+    partial class SeedUserRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
