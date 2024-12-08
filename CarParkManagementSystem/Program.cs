027b4e17e69e76b3ce244e76bcbc824cf1924cd6
noternestt <2305050c@student.tp.edu.sg>
2024-12-08 19:21:05 +0800

    added custom identification logic

--- CarParkManagementSystem/Program.cs
+++ CarParkManagementSystem/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<CarParkManagementSystemUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<CarParkManagementSystemContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
